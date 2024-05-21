85d354af48524b172a67653b0a1e0c41dc177f5c
Bibek Chaudhari <bibekchaudhary0728n@gmail.com>
2024-05-21 21:11:42 +0545

    Today i created Hero Section . In this i have created a Hero Section and button using Aceternity UI library.The button & Hero Section is looking slightly fashionable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from '@/components/herosection/HeroSection'
 import React from 'react'
+
 
 const Home = () => {
   return (
     <main className='min-h-screen bg-black/[0.9] antialiased bg-grid-white/[0.02]'>
-      <h1 className='text-3xl text-center text-black-500 bg-slate-200'>Hello Bibek</h1>
+      <HeroSection />
     </main>
   )
 }
